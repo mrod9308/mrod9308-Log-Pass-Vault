6f96e3ee57b61c38665ab40f86644e71acad32ff
Miguel Angel Rodriguez <mrod9308@gmail.com>
2020-03-31 20:54:07 -0700

    Update Password,login vault.py

--- Password,login vault.py
+++ Password,login vault.py
@@ -1,17 +1,15 @@
-o_start = input('To Start Press Enter: ')
-login_info = input('What is your login : ')
-password_info = input('What is your password : ')
-confirm = input('Is this correct please type Y for Yes ? ')
-
-
-def confirm_1():  #Confirms Value Y & outputs to user to confirm answer.
+def confirm_1():  # Confirms Value Y & outputs to user to confirm answer.
+    greet = 'Welcome to login Vault!'
+    print(greet)
+    to_start = input('To Start Press Enter: ')
+    login_info = input('What is your login : ')
+    password_info = input('What is your password : ')
+    confirm = input('Is this correct please type Y for Yes ? ')
     if confirm == str('Y'):
         print('Your Password & Login have been Securely stored !')
     elif confirm != 'Y':
         print('Try again, not Confirmed!!')
         print(0, 1, 2, 3, '.... out')
-        greet = 'Welcome to login Vault!'
-        print(greet)
         print('Password: ', password_info)
         print('login: ', login_info)
 
